36cf505c23dcae9bc8592b528609572eb8d79704
daida <daida459031925@163.com>
2020-05-29 13:30:56 +0800

    list的交集 并及 察冀

--- src/main/java/com/gitHub/past/listTool/ListUtil.java
+++ src/main/java/com/gitHub/past/listTool/ListUtil.java
@@ -2,27 +2,112 @@ package com.gitHub.past.listTool;
 
 import java.util.List;
 import java.util.function.BiFunction;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 //普通这样写是没有任何问题的但是对象的化如何对比
-public class ListUtil<T> {
+public class ListUtil<T,R,XT> {
+    //数据资源1
+    private List<T> zuo;
+    //数据资源2
+    private List<R> you;
+    //由于不知道是什么对象所以让用户自己传入转换后的
+    private Function<T,XT> fzuo;
+    //由于不知道是什么对象所以让用户自己传入转换后的
+    private Function<R,XT> fyou;
 
+    {
+        fzuo = null;
+        fyou = null;
+    }
+
+    /***
+     * @param zuo 基础数据list
+     * @param you 基础数据list
+     * @param fzuo 需要将list中zuo的数据变成一个新list   需要传入的就是java8中 流的map(e->XXXX)形式
+     * @param fyou 需要将list中you的数据变成一个新list   需要传入的就是java8中 流的map(e->XXXX)形式
+     */
+    public ListUtil(List<T> zuo, List<R> you, Function<T, XT> fzuo, Function<R, XT> fyou) {
+        this.zuo = zuo;
+        this.you = you;
+        this.fzuo = fzuo;
+        this.fyou = fyou;
+    }
+
+    /***
+     * @param zuo 基础数据list
+     * @param you 基础数据list
+     * @param fzuo 需要将list中zuo的数据变成一个新list   需要传入的就是java8中 流的map(e->XXXX)形式
+     * @param fyou 需要将list中you的数据变成一个新list   需要传入的就是java8中 流的map(e->XXXX)形式
+     */
+    public ListUtil<T,R,XT> ListUtilAll(List<T> zuo, List<R> you, Function<T, XT> fzuo, Function<R, XT> fyou) {
+        this.zuo = zuo;
+        this.you = you;
+        this.fzuo = fzuo;
+        this.fyou = fyou;
+        return this;
+    }
+
+    /***
+     * @param zuo 基础数据list
+     * @param you 基础数据list
+     * @param fyou 需要将list中you的数据变成一个新list   需要传入的就是java8中 流的map(e->XXXX)形式
+     */
+    public ListUtil<T,R,XT> ListUtilyou(List<T> zuo, List<R> you, Function<R, XT> fyou) {
+        this.zuo = zuo;
+        this.you = you;
+        this.fzuo = null;
+        this.fyou = fyou;
+        return this;
+    }
+
+    /***
+     * @param zuo 基础数据list
+     * @param you 基础数据list
+     * @param fzuo 需要将list中zuo的数据变成一个新list   需要传入的就是java8中 流的map(e->XXXX)形式
+     */
+    public ListUtil<T,R,XT> ListUtilzuo(List<T> zuo, List<R> you, Function<T, XT> fzuo) {
+        this.zuo = zuo;
+        this.you = you;
+        this.fzuo = fzuo;
+        this.fyou = null;
+        return this;
+    }
+
+    /***
+     * @param zuo 基础数据list
+     * @param you 基础数据list
+     */
+    public ListUtil<T,R,XT> ListUtil(List<T> zuo, List<R> you) {
+        this.zuo = zuo;
+        this.you = you;
+        this.fzuo = null;
+        this.fyou = null;
+        return this;
+    }
+
+    public void ajiaoJib(){
+        ajiaoJib.apply(zuo,you);
+    }
 
     //交集
-    public BiFunction<List<T>,List<T>,List<T>> jiaoJi = (List<T> i1, List<T> i2)->i1.stream().filter(num -> i2.contains(num))
-            .collect(Collectors.toList());;
+    private BiFunction<List<T>,List<R>,List<T>> ajiaoJib = (List<T> i1, List<R> i2)->i1.stream().filter(num -> i2.contains(num)).collect(Collectors.toList());;
+
+    private BiFunction<List<R>,List<T>,List<R>> bjiaoJia = (List<R> i2, List<T> i1)->i2.stream().filter(num -> i1.contains(num)).collect(Collectors.toList());;
 
     //差集
-    public BiFunction<List<T>,List<T>,List<T>> aJianb = (List<T> i1, List<T> i2)->i1.stream().filter(num -> !i2.contains(num))
-             .collect(Collectors.toList());
+    private BiFunction<List<T>,List<R>,List<T>> aJianb = (List<T> i1, List<R> i2)->i1.stream().filter(num -> !i2.contains(num)).collect(Collectors.toList());
+
+    //差集
+    private BiFunction<List<R>,List<T>,List<R>> bJiana = (List<R> i2 , List<T> i1)->i2.stream().filter(num -> !i1.contains(num)).collect(Collectors.toList());
 
     //并集  不去重
-    public BiFunction<List<T>,List<T>,List<T>> bingJiNoQuchong = (List<T> i1, List<T> i2)->{i1.addAll(i2);return i1;};
+    private BiFunction<List<Object>,List<Object>,List<Object>> bingJiNoQuchong = (List<Object> i1, List<Object> i2)->{i1.addAll(i2);return i1;};
 
     //并集  去重
-    public BiFunction<List<T>,List<T>,List<T>> bingJiQuchong = (List<T> i1, List<T> i2)->{
+    private BiFunction<List<Object>,List<Object>,List<Object>> bingJiQuchong = (List<Object> i1, List<Object> i2)->{
         i1.addAll(i2);
-        List<T> collect4 = i1.stream().distinct().collect(Collectors.toList());
+        List<Object> collect4 = i1.stream().distinct().collect(Collectors.toList());
         return collect4;
     };
 
--- src/test/java/testMain.java
+++ src/test/java/testMain.java
@@ -1,17 +1,29 @@
 import com.gitHub.past.Invariable;
 import com.gitHub.past.characterEncoding.CharEncoding;
+import com.gitHub.past.listTool.ListUtil;
+
+import java.util.Arrays;
+import java.util.List;
 
 
 public class testMain {
     public static void main(String[] args) {
-        System.out.println(Invariable.UTF_8);
-        System.out.println(Invariable.UTF_8.name());
-        System.out.println(Invariable.UTF_8.ordinal());
-        System.out.println(Invariable.UTF_8.toString());
-        System.out.println(Invariable.UTF_8.toString() == "UTF-8");
-
-
-        System.out.println(CharEncoding.codeing.apply("%3A%2F",Invariable.UTF_8));
+//        System.out.println(Invariable.UTF_8);
+//        System.out.println(Invariable.UTF_8.name());
+//        System.out.println(Invariable.UTF_8.ordinal());
+//        System.out.println(Invariable.UTF_8.toString());
+//        System.out.println(Invariable.UTF_8.toString() == "UTF-8");
+//
+//
+//        System.out.println(CharEncoding.codeing.apply("%3A%2F",Invariable.UTF_8));
 //                    .decode("&#39;&#34;&lt;&gt;",Invariable.UTF_8.toString());
+
+        List<Integer> integers = Arrays.asList(1, 2, 3);
+        List<Integer> integers1 = Arrays.asList(1, 2, 4);
+
+
+        ListUtil listUtil = new ListUtil(integers,integers1,null,null);
+        listUtil.aJianb.
+
     }
 }
